40133bf647bb4c0bc39300ed8965132dc10b26c2
Mr.ChenXin <2749871307@qq.com>
2020-10-11 20:41:57 +0800

    完成了登录和注册界面的开发

---
+++ assets/js/logincx.js
@@ -0,0 +1,63 @@
+$(function () {
+    $('#link-reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    });
+    $('#link-login').on('click', function () {
+        $('.reg-box').hide();
+        $('.login-box').show();
+    });
+
+    //自定义校验规则
+    //从Layui中获取form对象
+    var form = layui.form;
+    var layer = layui.layer;
+    //通过 form.verify() 函数自定义校验规则
+    form.verify({
+        // 自定义了一个叫做 pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致!';
+            }
+        }
+    })
+    //注册功能
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/reguser',
+            method: 'POST',
+            data: {
+                username: $('.reg-box [name=username]').val(),
+                password: $('.reg-box [name=password]').val(),
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('注册成功，请登录！');
+                $('#link-login').click();
+            }
+        })
+    })
+    $('#form_login').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败');
+                }
+                layer.msg('登录成功');
+                localStorage.setItem('token', res.token);
+                location.href = '/index.html';
+            }
+        })
+    })
+
+})
